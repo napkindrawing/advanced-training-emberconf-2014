cc13189db4ce3e8690bf210721f61f9616f7a9ff
fansipans <mike.walker@napkindrawing.com>
2014-03-24 11:24:53 -0700

    register store as injected service woohoo!

--- app/microdata/store.js
+++ app/microdata/store.js
@@ -1,1 +1,14 @@
-export default Ember.Object.extend();
+var store = Ember.Object.extend();
+
+Ember.Application.initializer({
+	name: 'microdata',
+
+	initialize: function(container, app) {
+		app.register('service:store', store);
+
+		app.inject('controller','store','service:store');
+	}
+
+});
+
+export default store;
